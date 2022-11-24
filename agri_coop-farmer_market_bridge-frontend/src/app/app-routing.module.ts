73de11edf5f0993e2831fd75940367c69a362d69
SayiaFelix <sayiafelix18@gmail.com>
2022-11-24 08:42:56 +0300

    adding signup form and validation

---
+++ agri_coop-farmer_market_bridge-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { RequestedProductsComponent } from './requested-products/requested-products.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  
+  {path:'homepage',component:HomepageComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path: 'requests', component: RequestedProductsComponent},
+  {path:'product',component:AllProductsComponent},
+  
+  {path:'**',component:NotfoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ agri_coop-farmer_market_bridge-frontend/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgToastModule } from 'ng-angular-popup';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component'
+import { RequestedProductsComponent } from './requested-products/requested-products.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { NgxMaskModule } from 'ngx-mask';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CartComponent } from './cart/cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    RequestedProductsComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    NotfoundComponent,
+    AllProductsComponent,
+    CartComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxMaskModule.forRoot(),
+    BrowserModule,
+    AppRoutingModule,
+    NgToastModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
